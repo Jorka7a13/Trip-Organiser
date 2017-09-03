3acf784a12ba5ae1e6d0068622781349cbc89bbd
George Tachev <g.tachew@gmail.com>
2017-09-03 18:07:30 +0300

    Work on the custom date and time picker

--- app/common/directives/date-time-picker-directive.js
+++ app/common/directives/date-time-picker-directive.js
@@ -8,11 +8,14 @@
 				return {
 					restrict: 'A',
 					link: function(scope, element, attrs) {
-		                $('#datetimepicker1').datetimepicker({
-		                    pick12HourFormat: scope.pick12HourFormat,
-		                    language: scope.language,
-		                    useCurrent: scope.useCurrent
-		                }); // Change '#datetimepicker1' to '[date-time-picker]'.
+		                // $('#datetimepicker1').datetimepicker({
+		                //     pick12HourFormat: scope.pick12HourFormat,
+		                //     language: scope.language,
+		                //     useCurrent: scope.useCurrent
+		                // }); // Change '#datetimepicker1' to '[date-time-picker]'.
+		                
+		                console.log($('#datetimepicker1'));
+		                console.log($('#datetimepicker1').datetimepicker);
 
 	             		element.on('blur', function() {
 			                // scope.departureDateTime = new Date(element.data('DateTimePicker').getDate().format());
